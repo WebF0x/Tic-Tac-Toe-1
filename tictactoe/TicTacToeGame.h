29a383c29417a0f4cfcda114e51db340cf82576c
Daan Davidsz <daandavidsz@xs4all.nl>
2011-05-23 21:51:02 +0200

    Fixed some stuff and added a Makefile

--- tictactoe/TicTacToeGame.h
+++ tictactoe/TicTacToeGame.h
@@ -9,42 +9,41 @@
 #define TICTACTOEGAME_H_
 
 #include <iostream>
-#include <string>
+#include <string.h>
 #include "Best.h"
 
-class TicTacToeGame{
-public:
-		enum Side { HUMAN, COMPUTER, EMPTY };
-	    enum PositionVal { HUMAN_WIN, DRAW, UNCLEAR, COMPUTER_WIN };
-		void setComputerPlays();
-		void setHumanPlays();
-		bool computerPlays();
-		int chooseMove();
-		bool moveOk(int move);
-		void playMove(int move);
-		std::string toString();
-		bool gameOver();
-		std::string winner();
-		int positionValue();
-		TicTacToeGame();
-		void move();
-		int side;
-		int position;
-		std::string humanChar;
-		std::string computerChar;
+class TicTacToeGame {
 
+    public:
+        enum Side { HUMAN, COMPUTER, EMPTY };
+        enum PositionVal { HUMAN_WIN, DRAW, UNCLEAR, COMPUTER_WIN };
+        TicTacToeGame(bool computerStarts);
+        void setComputerPlays();
+        void setHumanPlays();
+        bool computerPlays();
+        int chooseMove();
+        bool moveOk(int move);
+        void playMove(int move);
+        std::string toString();
+        bool gameOver();
+        std::string winner();
+        std::string getHumanChar();
+        std::string getComputerChar();        
+        int positionValue();
+        void move();
+        int side;
+        int position;
 
-private:
-		int board[3][3];
-		void initSide();
-		Best chooseMove(int side);
-		bool boardIsFull();
-		bool isAWin(int side);
-		void place(int row, int column, int piece);
-		bool squareIsEmpty(int row, int column);
-		void clearBoard();
-		int piece;
-
+    private:
+        bool computerStarts;
+        int board[3][3];
+        Best chooseMove(int side);
+        bool boardIsFull();
+        bool isAWin(int side);
+        void place(int row, int column, int piece);
+        bool squareIsEmpty(int row, int column);
+        void clearBoard();
+        int piece;
 };
 
 #endif /* TICTACTOEGAME_H_ */
